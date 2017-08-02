35356bd5ad714f29576327f420fbb13523482328
Ray Choi <raybchoi@gmail.com>
2017-08-02 12:34:40 -0700

    updated the JS file to accountant for Unit 8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
-console.log('User flipped a ' + cardTwo);
-console.log('User flipped a ' + cardFour);
+// holds all the cards
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = []; 
+var cardOne = cards[0];
+var cardTwo = cards[3];
+// assinging the latest cards in play
+cardsInPlay.push(cardOne, cardTwo);
+
+// testing which cards have been flipped
+console.log("User flipped cardOne " + cardOne);
+console.log("User flipped cardTwo " + cardTwo);
+// console.log('User filpped ' + cardsInPlay);
+
+// checking to see how many cards are in play - if two cards are in play
+// check to see if they match values
+// if they do return alert
+// if not then say other alert
+if ( cardsInPlay.length === 2 ) {
+	if ( cardsInPlay[0] === cardsInPlay[1] ) {
+		alert ('You found a match!');
+	} else {
+		alert ('Sorry, try again');
+	}
+} else {
+	alert ('Pick two cards');
+}
