03608dcb8c3a9e30bd31032b8b3eb37ce556c094
Ray Choi <raybchoi@gmail.com>
2017-08-02 17:20:57 -0700

    updated JS for unit 9

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-// holds all the cards
+// holds all the cards (these are global scope variables)
 var cards = ['queen', 'queen', 'king', 'king'];
-var cardsInPlay = []; 
-var cardOne = cards[0];
-var cardTwo = cards[3];
-// assinging the latest cards in play
-cardsInPlay.push(cardOne, cardTwo);
+var cardsInPlay = [];
 
-// testing which cards have been flipped
-console.log("User flipped cardOne " + cardOne);
-console.log("User flipped cardTwo " + cardTwo);
-// console.log('User filpped ' + cardsInPlay);
-
-// checking to see how many cards are in play - if two cards are in play
-// check to see if they match values
-// if they do return alert
-// if not then say other alert
-if ( cardsInPlay.length === 2 ) {
-	if ( cardsInPlay[0] === cardsInPlay[1] ) {
-		alert ('You found a match!');
-	} else {
-		alert ('Sorry, try again');
+// needs to come before the flipCard function and call otherwise will get an error
+// steps related to matching the two together
+var checkForMatch = function(){
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
-} else {
-	alert ('Pick two cards');
 }
+
+// function that stores all steps related to selecting / flipping over a card
+var flipCard = function(cardId) {
+	console.log('User flipped ' + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(0);
+flipCard(1);
